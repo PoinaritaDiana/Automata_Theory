9b9d5b7a2d40ba88b67b7a4b60c467b4b572e19f
PoinaritaDiana <61639111+PoinaritaDiana@users.noreply.github.com>
2020-05-05 18:04:04 +0300

    Add  eliminateTerminals

---
+++ CFG_CNF/CFG_CNF.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <map>
+#include <vector>]
+using namespace std;
+
+map <string, set<string>> P;	//Productii
+set <string> N;					//Neterminale
+set <char> Sigma;				//Terminale
+string S;
+char caracter;
+
+void configGrammar() {
+	P["S"].insert("aAb"); P["S"].insert("BC"); P["S"].insert("@");
+	P["A"].insert("AaB"); P["A"].insert("Db");
+	P["B"].insert("b"); P["B"].insert("C");
+	P["C"].insert("@"); P["C"].insert("ab");
+	P["D"].insert("@");
+	P["F"].insert("a");
+
+	N.insert("S"); N.insert("A"); N.insert("B");
+	N.insert("C"); N.insert("D"); N.insert("F");
+	Sigma.insert('a'); Sigma.insert('b');
+	S = "S";
+
+	caracter = 'F';
+}
+
+void eliminateStart() {
+	N.insert("S0");
+	S = "S0";
+	P["S0"].insert("S");
+}
+
+void lambdaProductions() {
+	set <string> eliminare;
+	//Pentru fiecare neterminal
+	for (auto n : N) {
+		set<string>::iterator it;
+		it = P[n].find("@");
+		//Daca se gaseste lambda
+		if (it != P[n].end()) {
+			if (P[n].size() == 1) eliminare.insert(n);
+		}
+	}
+}
+
+void unitProductions() {
+
+}
+
+void uselessProductions() {
+
+}
+
+void eliminateTerminals() {
+	//Productiile noi adaugate
+	set <pair<char, char>> newAdd;
+
+	for (auto n : N) {
+		set<string> Pnou;	//Retin toate productiile (cu modificari, daca e cazul)
+		for (auto p : P[n]) {
+			if (p.size() >= 2) {
+				for (int i = 0; i < p.size(); i++) {
+					if (Sigma.find(p[i]) != Sigma.end()) {
+						int ok = 0;
+						char caracternou;
+						for (auto s : newAdd)
+							if (s.second == p[i]) {
+								caracternou = s.first;
+								ok = 1;
+								break;
+							}
+						if (ok == 1)
+							p[i] = caracternou;
+						else {
+							caracter++;
+							string sc = string(1, caracter);
+							P[sc].insert(string(1, p[i]));
+							newAdd.insert({ caracter,p[i] });
+							p[i] = caracter;
+						}
+					}
+				}
+			}
+			Pnou.insert(p);
+		}
+		P[n] = Pnou;
+	}
+
+	//Adaug neterminalele noi
+	if (newAdd.size() != 0) {
+		for (auto s : newAdd)
+			N.insert(string(1, s.first));
+	}
+}
+
+void eliminateMoreNonTerminals() {
+
+}
+
+
+
+int main() {
+	configGrammar();
+	eliminateTerminals();
+
+	for (auto p : P) {
+		cout << p.first << "->";
+		for (auto e : p.second)cout << e << ",";
+		cout << endl;
+	}
+
+	return 0;
+}
